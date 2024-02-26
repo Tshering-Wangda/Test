def6ea9c314dc13874b822a02c63ff84d1e57133
Tshering Wangda <02230113.cst@rub.edu.bt>
2024-02-26 14:57:17 +0600

    logical.py

---
+++ logical.py
@@ -0,0 +1,20 @@
+# AND Operator(Both conditions should be true)
+x = 5
+print(x > 3 and x < 10)
+
+y = 12
+print(y > 10 and y % 5 == 0)
+
+# OR Operator(True if one of the condition is true)
+x = 5
+print(x < 3 or x > 10)
+
+y = 12
+print(y < 10 or y % 2 == 0)
+
+# NOT Operator(Reverse)
+x = 5
+print(not(x > 3 and x < 10))
+
+y = 12
+print(not(y > 10 and y % 5 == 0))
